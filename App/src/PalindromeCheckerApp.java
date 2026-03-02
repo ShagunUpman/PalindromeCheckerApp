98f705100d6e33fd817310b453a2d13b5fb35c0c
ShagunUpman <shagunupman@gmail.com>
2026-03-02 14:35:52 +0530

    UseCase2PalindromeCheckerApp

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,10 +1,39 @@
-public class UseCase1PalindromeCheckerApp {
+/**
+ * ==============================================================
+ * MAIN CLASS - UseCase2PalindromeCheckerApp
+ * ==============================================================
+ *
+ * Use Case 2: Hardcoded Palindrome Validation
+ *
+ * Description:
+ * This class demonstrates basic palindrome validation
+ * using a hardcoded string value.
+ *
+ * At this stage, the application:
+ * - Stores a predefined string
+ * - Compares characters from both ends
+ * - Determines whether the string is a palindrome
+ * - Displays the result on the console
+ */
+public class UseCase2PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("====================================");
-        System.out.println(" Welcome to Palindrome Checker App ");
-        System.out.println(" Version: 1.0.2 (UC1)");
-        System.out.println(" Module: Application Entry & Welcome Message");
-        System.out.println("====================================");
-        System.out.println("Application started successfully.");
+        String inputText = "madam";
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = inputText.length() - 1;
+
+        while (left < right) {
+            if (inputText.charAt(left) != inputText.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println("Input text: " + inputText);
+        System.out.println("Is it a palindrome? : " + isPalindrome);
     }
 }
